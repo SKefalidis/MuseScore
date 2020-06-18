effb8dd8a508068857ef3a986d4946562286b79b
SKefalidis <megistios@gmail.com>
2020-06-18 13:59:06 +0300

    update toolbar for the selected movement, not the first one

--- mscore/musescore.cpp
+++ mscore/musescore.cpp
@@ -6409,7 +6409,7 @@ void MuseScore::endCmd(const bool isCmdFromInspector, const bool undoRedo)
     }
     if (cs) {
         setPos(cs->inputState().tick());
-        updateInputState(cs);
+        updateInputState(cv->score());
         updateUndoRedo();
         dirtyChanged(cs);
         scoreCmpTool->updateDiff();
