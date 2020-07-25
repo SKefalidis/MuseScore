bd0a49eeb81d5e677c95258bac8c2d488ae0f501
SKefalidis <megistios@gmail.com>
2020-07-25 18:12:32 +0300

    add/remove score test

--- mtest/libmscore/albumsIO/tst_albumsIO.cpp
+++ mtest/libmscore/albumsIO/tst_albumsIO.cpp
@@ -37,12 +37,14 @@ class TestAlbumsIO : public QObject, public MTest
 
     void saveAlbumTest(const char* file);
     void stringsTest(const char* file);
+    void addRemoveTest(const char* file);
 
 private slots:
     void initTestCase();
 
     void albumsIO() { saveAlbumTest("smallPianoAlbumTest"); };
     void albumsStrings() { stringsTest("smallPianoAlbumTest"); };
+    void albumsAddRemove() { addRemoveTest("smallPianoAlbumTest"); };
 };
 
 //---------------------------------------------------------
@@ -79,6 +81,7 @@ void TestAlbumsIO::stringsTest(const char* file)
     MScore::debugMode = true;
     Album* album = readAlbum(DIR + QString(file) + ".msca");
     QVERIFY(album);
+
     auto x = album->composers();
     QVERIFY(x.size() == 2);
     QCOMPARE(x.at(0), "Sergios - Anestis Kefalidis");
@@ -91,6 +94,40 @@ void TestAlbumsIO::stringsTest(const char* file)
     QCOMPARE(z.at(0), "Piano 1");
     QCOMPARE(z.at(1), "Piano 2");
     QCOMPARE(z.at(2), "Piano 3");
+
+    album->removeScore(0);
+
+    auto x2 = album->composers();
+    QVERIFY(x2.size() == 1);
+    QCOMPARE(x2.at(0), "Oregano");
+    auto y2 = album->composers();
+    QVERIFY(y2.size() == 1);
+    QCOMPARE(y2.at(0), "Garlic");
+    auto z2 = album->scoreTitles();
+    QVERIFY(z2.size() == 2);
+    QCOMPARE(z2.at(1), "Piano 2");
+    QCOMPARE(z2.at(2), "Piano 3");
+
+    delete album;
+}
+
+//---------------------------------------------------------
+//   addRemoveTest
+//---------------------------------------------------------
+
+void TestAlbumsIO::addRemoveTest(const char* file)
+{
+    MScore::debugMode = true;
+    Album* album = readAlbum(DIR + QString(file) + ".msca");
+    QVERIFY(album);
+
+    QVERIFY(album->albumItems().size() == 3);
+    MasterScore* ms = album->albumItems().at(1)->score;
+    album->removeScore(ms);
+    QVERIFY(album->albumItems().size() == 2);
+    album->addScore(ms);
+    QVERIFY(album->albumItems().size() == 3);
+
     delete album;
 }
 
